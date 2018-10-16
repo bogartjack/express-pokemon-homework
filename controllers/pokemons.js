84f3bffea7c2563d5fffc1c46b50aa9be15d779d
Jack Bogart <bogart.jack98@gmail.com>
2018-10-16 15:46:52 -0600

    commit 6-new route created

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Pokemon = require('../models/pokemon.js');
 
-router.get('/', (req, res) =>{
-	res.render('index.ejs', {pokemon: Pokemon});
+router.get('/new', (req, res) =>{
+	res.render('new.ejs', {pokemon: Pokemon});
 });
 
 router.get('/:id', (req, res) =>{
 	res.render('show.ejs', {which: Pokemon[req.params.id]});
 });	
+
+router.get('/', (req, res) =>{
+	res.render('index.ejs', {pokemon: Pokemon});
+});
+
+router.post('/', (req, res) => {
+	Pokemon.push(req.body);
+	res.redirect('/pokemon/'+(Pokemon.length-1));
+});
 
 module.exports = router;
