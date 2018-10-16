50c5b668522237ad8a3c753d25b0e5ca837b6a14
Jack Bogart <bogart.jack98@gmail.com>
2018-10-16 15:03:44 -0600

    can see json

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const Pokemon = require('../models/pokemon.js');
+
+router.get('/', (req, res) =>{
+	res.send(Pokemon);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const pokemonController = require('./controllers/pokemons');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
-app.use('/pokemon/', pokemonController);
+app.use('/pokemon', pokemonController);
 
 app.listen(port, ()=>{
 	console.log('app is listening');
