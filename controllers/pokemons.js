b5625385b14dac2dc2e7923ae4a6af22d7eddded
Jack Bogart <bogart.jack98@gmail.com>
2018-10-16 16:05:35 -0600

    commit 7-edit route

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -10,8 +10,18 @@ router.get('/:id', (req, res) =>{
 	res.render('show.ejs', {which: Pokemon[req.params.id]});
 });	
 
+router.get('/:id/edit', (req, res) =>{
+	res.render('edit.ejs', {which: Pokemon[req.params.id], index: req.params.id});
+});
+
 router.get('/', (req, res) =>{
 	res.render('index.ejs', {pokemon: Pokemon});
+});
+
+router.put('/:id', (req, res) =>{
+	Pokemon[req.params.id].name = req.body.name;
+	Pokemon[req.params.id].img = req.body.img;
+	res.redirect('/pokemon/'+req.params.id);
 });
 
 router.post('/', (req, res) => {
